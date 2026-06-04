820aa92a4cb5614ba36f7e414cb3053e36465c0e
Cuddliness <herex@protonmail.com>
2026-06-04 15:03:17 +0200

    Improved string utils and pregnant reaction on barb mention

--- src/main/java/care/cuddliness/djcarl/listeners/MessageCarlResponseListener.java
+++ src/main/java/care/cuddliness/djcarl/listeners/MessageCarlResponseListener.java
@@ -18,7 +18,7 @@ public class MessageCarlResponseListener extends ListenerAdapter {
         String[] split = event.getMessage().getContentRaw().split(" ");
         for(String s : split){
             StringUtil util = new StringUtil(s.toLowerCase());
-            if(util.isSimilar("DJCARL") || event.getMessage().getContentRaw().contains("DJ CARL")){
+            if(util.isSimilar("Carl")){
                 event.getMessage().reply("THAT IS ME! DJ CARLLLLLL!!!").queue();
                 return;
             }
@@ -35,9 +35,17 @@ public class MessageCarlResponseListener extends ListenerAdapter {
                 event.getMessage().reply("SLAYERRRRRRRR!!").queue();
                 return;
             }
-            if(util.isSimilar("feet") || event.getMessage().getContentRaw().toLowerCase().contains("feet")){
+            if(util.isSimilar("feet") || event.getMessage().getContentRaw().toLowerCase().contains("feet")) {
                 event.getMessage().addReaction(Emoji.fromUnicode("\uD83E\uDDB6")).queue();
                 return;
+            }
+            if(!event.getMessage().getMentions().getMembers().isEmpty()) {
+                event.getMessage().getMentions().getMembers().forEach(member -> {
+                    System.out.println(member.getUser().getIdLong());
+                    if (member.getUser().getIdLong() == 228154846828560384L) {
+                        event.getMessage().addReaction(Emoji.fromUnicode("U+1F930")).queue();
+                    }
+                });
             }
 
         }
--- src/main/java/care/cuddliness/djcarl/utils/StringUtil.java
+++ src/main/java/care/cuddliness/djcarl/utils/StringUtil.java
@@ -6,67 +6,59 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class StringUtil {
-    private static final float THRESHOLD = (float) 0.75;
-
-    private final Logger logger = Logger.getLogger(StringUtil.class.getName());
-
     private String str;
-    private Map <Character, Integer> strMap;
+    private Map<Character, Integer> strMap;
+    private Pattern wordPattern;
+    private static final float THRESHOLD = 0.8f;
+
     public StringUtil() {}
 
-    public StringUtil(String str){ //java.lang.String is final...
+    public StringUtil(String str) {
         this.str = str;
         this.strMap = this.generateCharMap(str);
+        this.wordPattern = Pattern.compile(
+                "(?<![\\w])(" + Pattern.quote(str) + ")(?![\\w])",
+                Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CHARACTER_CLASS
+        );
     }
-    private Map <Character, Integer> generateCharMap(String str){
-        Map <Character, Integer> map = new HashMap<>();
-        Integer currentChar;
-        for(char c: str.toCharArray()){
-            currentChar = map.get(c);
-            if(currentChar == null){
-                map.put(c, 1);
-            } else {
-                map.put(c, currentChar+1);
-            }
+
+    private Map<Character, Integer> generateCharMap(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            map.merge(c, 1, Integer::sum);
         }
         return map;
     }
 
-    public boolean isSimilar(String compareStr){
+    public boolean isSimilar(String compareStr) {
+        if (compareStr == null || compareStr.isEmpty()) return false;
+
+        String reversed = new StringBuilder(compareStr).reverse().toString();
+        if (reversed.equalsIgnoreCase(this.str)) return false;
+
+        float lengthRatio = (float) Math.min(this.str.length(), compareStr.length()) /
+                (float) Math.max(this.str.length(), compareStr.length());
+        if (lengthRatio < 0.6f) return false;
+
+        if (wordPattern.matcher(compareStr).find()) return true;
+
         Map<Character, Integer> compareStrMap = this.generateCharMap(compareStr);
-        Set<Character> charSet = compareStrMap.keySet();
         int similarChars = 0;
-        int totalStrChars = this.str.length();
-        float thisThreshold;
+        int totalStrChars = Math.max(this.str.length(), compareStr.length());
 
-        if(totalStrChars < compareStrMap.size()){
-            totalStrChars = compareStr.length();
-        }
-
-        Iterator it = charSet.iterator();
-        char currentChar;
-        Integer currentCountStrMap;
-        Integer currentCountCompareStrMap;
-        while(it.hasNext()){
-            currentChar = (Character)it.next();
-            currentCountStrMap = strMap.get(currentChar);
-            if(currentCountStrMap != null){
-                currentCountCompareStrMap = compareStrMap.get(currentChar);
-                if (currentCountCompareStrMap >= currentCountStrMap){
-                    similarChars += currentCountStrMap;
-                } else {
-                    similarChars += currentCountCompareStrMap;
-                }
+        for (char c : compareStrMap.keySet()) {
+            Integer inBase    = strMap.get(c);
+            Integer inCompare = compareStrMap.get(c);
+            if (inBase != null) {
+                similarChars += Math.min(inBase, inCompare);
             }
         }
 
-        thisThreshold = ((float) similarChars)/((float) totalStrChars);
-        Logger.getLogger(StringUtil.class.getName()).log(Level.INFO, "similarChars: {0}, totalStrChars: {1}, thisThreshold: {2}", new Object[]{similarChars, totalStrChars, thisThreshold});
-        if(thisThreshold > THRESHOLD){
-            return true;
-        }
-        return false;
+        float thisThreshold = (float) similarChars / (float) totalStrChars;
+
+        return thisThreshold > THRESHOLD;
     }
 }
